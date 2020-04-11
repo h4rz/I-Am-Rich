2f3b8ee8b04246b84a6c02714f94971e93d35bcb
Harsh Rajgor <harshrajgor996@gmail.com>
2020-04-11 10:38:55 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Harsh Rajgor on 05/04/20.
+//  Copyright © 2020 Harsh Rajgor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
